27f0deafac4bbb55ba160bfe263f1ff9a4d2b94c
kirillovdm <kyrylovdm@gmail.com>
2016-03-24 11:28:47 +0200

    content update

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -88,11 +88,40 @@ $(window).on('load', function () {
         childrenSlider.closest('.bx-wrapper').addClass('open');
     });
 
+    var isPC;
+    if(navigator.userAgent.indexOf('Mac OS X') != -1){
+      // for Mac
+      isPC = false;
+    }
+    else {
+      // for PC
+      isPC = true;
+    }
+
+    if (isPC){
+        $('.h-scroll-cont').addClass('addmargin');
+    }
+
+
 })(window.jQuery);
 
- $('.carousel').carousel({
-        interval: false
-    });
+
+$(function() {
+
+   $(".timeline-block").mousewheel(function(event, delta) {
+
+      this.scrollLeft -= (delta * 30);
+    
+      event.preventDefault();
+
+   });
+
+});
+
+$('.carousel').carousel({
+    interval: false
+});
+
 $(document).on("change", ".file-input", function(){
 
         var $fileLabel = $(".drop-text");
@@ -191,14 +220,18 @@ $(document).ready(function(){
     minSlides: 3,
     maxSlides: 8,
     moveSlides: 2,
-    slideMargin: 20
+    slideMargin: 20,
+    infiniteLoop: false,
+    hideControlOnEnd: true
   });
   $('.v-card-slider').bxSlider({
     slideWidth: 320,
     minSlides: 3,
     maxSlides: 8,
     moveSlides: 1,
-    slideMargin: 20
+    slideMargin: 20,
+    infiniteLoop: false,
+    hideControlOnEnd: true
   });
   $('.team-slider').bxSlider({
     slideWidth: 600,
@@ -217,44 +250,87 @@ $(document).on('click','.head-arrow-down, .learn-about-link', function(e){
 });
 
 /*jQuery BEZ Plugin for cubic-bezier easing*/
-        jQuery.extend({ bez: function(encodedFuncName, coOrdArray) {
-            if (jQuery.isArray(encodedFuncName)) {
-                coOrdArray = encodedFuncName;
-                encodedFuncName = 'bez_' + coOrdArray.join('_').replace(/\./g, 'p');
-            }
-            if (typeof jQuery.easing[encodedFuncName] !== "function") {
-                var polyBez = function(p1, p2) {
-                    var A = [null, null], B = [null, null], C = [null, null],
-                        bezCoOrd = function(t, ax) {
-                            C[ax] = 3 * p1[ax], B[ax] = 3 * (p2[ax] - p1[ax]) - C[ax], A[ax] = 1 - C[ax] - B[ax];
-                            return t * (C[ax] + t * (B[ax] + t * A[ax]));
-                        },
-                        xDeriv = function(t) {
-                            return C[0] + t * (2 * B[0] + 3 * A[0] * t);
-                        },
-                        xForT = function(t) {
-                            var x = t, i = 0, z;
-                            while (++i < 14) {
-                                z = bezCoOrd(x, 0) - t;
-                                if (Math.abs(z) < 1e-3) break;
-                                x -= z / xDeriv(x);
-                            }
-                            return x;
-                        };
-                        return function(t) {
-                            return bezCoOrd(xForT(t), 1);
-                        }
-                };
-                jQuery.easing[encodedFuncName] = function(x, t, b, c, d) {
-                    return c * polyBez([coOrdArray[0], coOrdArray[1]], [coOrdArray[2], coOrdArray[3]])(t/d) + b;
+jQuery.extend({ bez: function(encodedFuncName, coOrdArray) {
+    if (jQuery.isArray(encodedFuncName)) {
+        coOrdArray = encodedFuncName;
+        encodedFuncName = 'bez_' + coOrdArray.join('_').replace(/\./g, 'p');
+    }
+    if (typeof jQuery.easing[encodedFuncName] !== "function") {
+        var polyBez = function(p1, p2) {
+            var A = [null, null], B = [null, null], C = [null, null],
+            bezCoOrd = function(t, ax) {
+                C[ax] = 3 * p1[ax], B[ax] = 3 * (p2[ax] - p1[ax]) - C[ax], A[ax] = 1 - C[ax] - B[ax];
+                return t * (C[ax] + t * (B[ax] + t * A[ax]));
+            },
+            xDeriv = function(t) {
+                return C[0] + t * (2 * B[0] + 3 * A[0] * t);
+            },
+            xForT = function(t) {
+                var x = t, i = 0, z;
+                while (++i < 14) {
+                    z = bezCoOrd(x, 0) - t;
+                    if (Math.abs(z) < 1e-3) break;
+                    x -= z / xDeriv(x);
                 }
+                return x;
+            };
+            return function(t) {
+                return bezCoOrd(xForT(t), 1);
             }
-            return encodedFuncName;
-        }});
-        /*jQuery BEZ Plugin for cubic-bezier easing END*/
+        };
+        jQuery.easing[encodedFuncName] = function(x, t, b, c, d) {
+            return c * polyBez([coOrdArray[0], coOrdArray[1]], [coOrdArray[2], coOrdArray[3]])(t/d) + b;
+        }
+    }
+    return encodedFuncName;
+}});
+/*jQuery BEZ Plugin for cubic-bezier easing END*/
 
 $(document).on('click', '.affix li a', function(){
     $('.affix a.active').removeClass('active');
     $(this).addClass('active');
+});
+
+
+// var scrollCounter;
+var $hInner = $('.h-scroll-inner');
+var $hOuter = $('.h-scroll-cont');
+var $arrowPrev = $('.white-arrow.prev');
+var $arrowNext = $('.white-arrow.next');
+var $animObject = $('.evolution-text');
+var lt, rt;
+
+$(document).on('click', '.white-arrow.next', function(){
+
+    var leftPos = $hOuter.scrollLeft();
+    rt = $hOuter.width() - leftPos;
+
+    if(rt < 260){
+        $arrowNext.addClass('hidden');
+    }
+    else if(leftPos > -1){
+        $arrowPrev.removeClass('hidden');
+    }
+
+    $hOuter.animate({scrollLeft:leftPos+150}, 200);
 
 });
+
+$(document).on('click', '.white-arrow.prev', function(){
+    var leftPos = $hOuter.scrollLeft();
+    lt = $hOuter.width() - leftPos;
+
+    if(leftPos < 150){
+        $arrowPrev.addClass('hidden');
+    }
+    else if(lt > 120){
+        $arrowNext.removeClass('hidden');
+    }
+
+    $hOuter.animate({scrollLeft:leftPos-150}, 200);
+
+    
+});
+
+var graph = Snap('#total-path-1');
+
