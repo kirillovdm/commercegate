1bd03fee2b2d93a9387c4988d201430d73f65cba
kirillovdm <kyrylovdm@gmail.com>
2016-04-06 16:37:51 +0300

    up

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -111,61 +111,6 @@ $(window).on('load', function () {
         $('.h-scroll-cont').addClass('addmargin');
     }
 
-    // var data = {
-    //     labels: ["2006", "2009", "2012", "2014", "2015", "2016"],
-    //     datasets: [
-    //         {
-    //             label: "My First dataset",
-    //             fillColor: "rgba(73,174,234,1)",
-    //             strokeColor: "rgba(124,247,139,1)",
-    //             highlightFill: "rgba(73,174,234,1)",
-    //             highlightStroke: "rgba(73,174,234,1",
-    //             data: [65, 59, 80, 81, 56, 55, 40]
-    //         }
-    //     ]
-    // };
-
-    // var options = {
-    //         //Boolean - Whether the scale should start at zero, or an order of magnitude down from the lowest value
-    //         scaleBeginAtZero : true,
-
-    //         //Boolean - Whether grid lines are shown across the chart
-    //         scaleShowGridLines : true,
-
-    //         //String - Colour of the grid lines
-    //         scaleGridLineColor : "rgba(0,0,0,.05)",
-
-    //         //Number - Width of the grid lines
-    //         scaleGridLineWidth : 1,
-
-    //         //Boolean - Whether to show horizontal lines (except X axis)
-    //         scaleShowHorizontalLines: true,
-
-    //         //Boolean - Whether to show vertical lines (except Y axis)
-    //         scaleShowVerticalLines: true,
-
-    //         //Boolean - If there is a stroke on each bar
-    //         barShowStroke : true,
-
-    //         //Number - Pixel width of the bar stroke
-    //         barStrokeWidth : 2,
-
-    //         //Number - Spacing between each of the X value sets
-    //         barValueSpacing : 5,
-
-    //         //Number - Spacing between data sets within X values
-    //         barDatasetSpacing : 1,
-
-    //         //String - A legend template
-    //         legendTemplate : "<ul class=\"<%=name.toLowerCase()%>-legend\"><% for (var i=0; i<datasets.length; i++){%><li><span style=\"background-color:<%=datasets[i].fillColor%>\"></span><%if(datasets[i].label){%><%=datasets[i].label%><%}%></li><%}%></ul>"
-
-    //     }
-    // var ctx = document.getElementById("ec-graph").getContext("2d");
-    // var myBarChart = new Chart(ctx).Bar(data, options);
-
-
-
-
 })(window.jQuery);
 
 
@@ -306,59 +251,126 @@ $(document).on('scroll onload', function(){
 
         checkAnimations();
 
+
+
 });
 
 
 $(document).ready(function(){
-  $('.fullscreen-slider').bxSlider({
-    slideWidth: 430,
-    minSlides: 4,
-    maxSlides: 5,
-    moveSlides: 2,
-    slideMargin: 10
-  });
 
-  $('.popular-products-slider').bxSlider({
-    slideWidth: 300,
-    minSlides: 3,
-    maxSlides: 3,
-    moveSlides: 3,
-    slideMargin: 20
-  });
+    var isMobile = false; //initiate as false
+    // device detection
+    if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|ipad|iris|kindle|Android|Silk|lge |maemo|midp|mmp|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows (ce|phone)|xda|xiino/i.test(navigator.userAgent) 
+    || /1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(navigator.userAgent.substr(0,4))) isMobile = true;
+    if (!isMobile) {
+      $('.fullscreen-slider').bxSlider({
+            slideWidth: 430,
+            minSlides: 4,
+            maxSlides: 5,
+            moveSlides: 2,
+            slideMargin: 10
+          });
 
-  $('.carousel-small-3').bxSlider({
-    slideWidth: 220,
-    minSlides: 3,
-    maxSlides: 3,
-    moveSlides: 3,
-    slideMargin: 20
-  });
+          $('.popular-products-slider').bxSlider({
+            slideWidth: 300,
+            minSlides: 3,
+            maxSlides: 3,
+            moveSlides: 3,
+            slideMargin: 20
+          });
 
-  $('.career-slider').bxSlider({
-    slideWidth: 290,
-    minSlides: 3,
-    maxSlides: 8,
-    moveSlides: 2,
-    slideMargin: 20,
-    infiniteLoop: false,
-    hideControlOnEnd: true
-  });
-  $('.v-card-slider').bxSlider({
-    slideWidth: 320,
-    minSlides: 3,
-    maxSlides: 8,
-    moveSlides: 1,
-    slideMargin: 20,
-    infiniteLoop: false,
-    hideControlOnEnd: true
-  });
-  $('.team-slider').bxSlider({
-    slideWidth: 600,
-    minSlides: 2,
-    maxSlides: 10,
-    moveSlides: 1,
-    slideMargin: 20
-  });
+          $('.carousel-small-3').bxSlider({
+            slideWidth: 220,
+            minSlides: 3,
+            maxSlides: 3,
+            moveSlides: 3,
+            slideMargin: 20
+          });
+
+          $('.career-slider').bxSlider({
+            slideWidth: 290,
+            minSlides: 3,
+            maxSlides: 8,
+            moveSlides: 2,
+            slideMargin: 20,
+            infiniteLoop: false,
+            hideControlOnEnd: true
+          });
+          $('.v-card-slider').bxSlider({
+            slideWidth: 320,
+            minSlides: 3,
+            maxSlides: 8,
+            moveSlides: 1,
+            slideMargin: 20,
+            infiniteLoop: false,
+            hideControlOnEnd: true
+          });
+          $('.team-slider').bxSlider({
+            slideWidth: 600,
+            minSlides: 2,
+            maxSlides: 10,
+            moveSlides: 1,
+            slideMargin: 20
+          });
+          console.log('nomob');
+    }
+
+    else{
+
+        $('.wow').removeClass('wow' );
+
+        $('.fullscreen-slider').bxSlider({
+            slideWidth: 300,
+            minSlides: 1,
+            maxSlides: 5,
+            moveSlides: 1,
+            slideMargin: 10
+          });
+
+          $('.popular-products-slider').bxSlider({
+            slideWidth: 300,
+            minSlides: 1,
+            maxSlides: 3,
+            moveSlides: 1,
+            slideMargin: 20
+          });
+
+          $('.carousel-small-3').bxSlider({
+            slideWidth: 300,
+            minSlides: 1,
+            maxSlides: 3,
+            moveSlides: 1,
+            slideMargin: 20
+          });
+
+          $('.career-slider').bxSlider({
+            slideWidth: 300,
+            minSlides: 1,
+            maxSlides: 8,
+            moveSlides: 1,
+            slideMargin: 20,
+            infiniteLoop: false,
+            hideControlOnEnd: true
+          });
+          $('.v-card-slider').bxSlider({
+            slideWidth: 300,
+            minSlides: 1,
+            maxSlides: 8,
+            moveSlides: 1,
+            slideMargin: 20,
+            infiniteLoop: false,
+            hideControlOnEnd: true
+          });
+          $('.team-slider').bxSlider({
+            slideWidth: 300,
+            minSlides: 1,
+            maxSlides: 10,
+            moveSlides: 1,
+            slideMargin: 20
+          });
+
+          console.log('mob');
+    }
 
 });
 
@@ -631,5 +643,5 @@ $(document).on('hover', '.fae-c', function(){
 // $("#ellipse3").hover(over, out);
 
 $(document).on('click tap', '.menu-icon', function(){
-    $('.right-block, .menu-icon, body').toggleClass('active');
+    $('.layout-header, .menu-icon, .content-wrapper').toggleClass('active');
 });
